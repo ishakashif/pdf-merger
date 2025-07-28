3d1946ed72a5a55d5f08f79c656644b2b87155df
Isha Kashif <ishak@udel.edu>
2025-07-28 16:11:09 +0500

    sorted pdf files in alphabetical order

--- pdfMerger.py
+++ pdfMerger.py
@@ -7,7 +7,7 @@ merger = PyPDF2.PdfFileMerger()
 pdf_folder = 'pdf-files'
 
 
-for file in os.listdir(pdf_folder):
+for file in sorted(os.listdir(pdf_folder)):
     if file.endswith('.pdf'):
         filepath = os.path.join(pdf_folder, file)
         print(f"Merging: {filepath}")
